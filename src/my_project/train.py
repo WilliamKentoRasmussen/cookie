a3e8b0c44189eba8e6999b3bab06cd955faedefc
WilliamKentoRasmussen <s245310@dtu.dk>
2026-01-06 12:00:54 +0100

    First commit

---
+++ src/my_project/train.py
@@ -0,0 +1,10 @@
+from my_project.model import Model
+from my_project.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
