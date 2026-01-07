638b9e2a4a4da02f7a216e86c900ccbf63bf8e81
WilliamKentoRasmussen <s245310@dtu.dk>
2026-01-07 15:51:27 +0100

    Lets go

--- src/my_project/data.py
+++ src/my_project/data.py
@@ -1,29 +1,59 @@
-from pathlib import Path
-
+import torch
 import typer
-from torch.utils.data import Dataset
 
 
-class MyDataset(Dataset):
-    """My custom dataset."""
+def normalize(images: torch.Tensor) -> torch.Tensor:
+    """Normalize images."""
+    #normalizes the images to have zero mean and unit variance. Images are 2d 
+    return (images - images.mean()) / images.std()
 
-    def __init__(self, data_path: Path) -> None:
-        self.data_path = data_path
 
-    def __len__(self) -> int:
-        """Return the length of the dataset."""
+def preprocess_data(raw_dir: str, processed_dir: str) -> None:
+    """Process raw data and save it to processed directory."""
 
-    def __getitem__(self, index: int):
-        """Return a given sample from the dataset."""
+    # Load raw data from each file
+    train_images, train_target = [], []
+    for i in range(6):
+        train_images.append(torch.load(f"{raw_dir}/train_images_{i}.pt"))
+        train_target.append(torch.load(f"{raw_dir}/train_target_{i}.pt"))
 
-    def preprocess(self, output_folder: Path) -> None:
-        """Preprocess the raw data and save it to the output folder."""
+    #concatenate the loaded tensors into single tensors that is a 3d tensor
+    train_images = torch.cat(train_images)
+    train_target = torch.cat(train_target)
 
-def preprocess(data_path: Path, output_folder: Path) -> None:
-    print("Preprocessing data...")
-    dataset = MyDataset(data_path)
-    dataset.preprocess(output_folder)
+    # Load test data
+    test_images: torch.Tensor = torch.load(f"{raw_dir}/test_images.pt")
+    test_target: torch.Tensor = torch.load(f"{raw_dir}/test_target.pt")
+
+    train_images = train_images.unsqueeze(1).float()
+    test_images = test_images.unsqueeze(1).float()
+    #Before: [N, 28, 28]
+    #After:  [N, 1, 28, 28]
+    train_target = train_target.long()
+    test_target = test_target.long() #Converts labels to integer class indices (required for CrossEntropyLoss).
+    
+
+    train_images = normalize(train_images)
+    test_images = normalize(test_images)
+
+    #Writes cleaned tensors to the processed_dir.
+    torch.save(train_images, f"{processed_dir}/train_images.pt")
+    torch.save(train_target, f"{processed_dir}/train_target.pt")
+    torch.save(test_images, f"{processed_dir}/test_images.pt")
+    torch.save(test_target, f"{processed_dir}/test_target.pt")
+
+
+def corrupt_mnist() -> tuple[torch.utils.data.Dataset, torch.utils.data.Dataset]:
+    """Return train and test datasets for corrupt MNIST."""
+    train_images = torch.load("data/processed/train_images.pt")
+    train_target = torch.load("data/processed/train_target.pt")
+    test_images = torch.load("data/processed/test_images.pt")
+    test_target = torch.load("data/processed/test_target.pt")
+
+    train_set = torch.utils.data.TensorDataset(train_images, train_target)
+    test_set = torch.utils.data.TensorDataset(test_images, test_target)
+    return train_set, test_set
 
 
 if __name__ == "__main__":
-    typer.run(preprocess)
+    typer.run(preprocess_data)
