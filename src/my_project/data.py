4641f668bb6840ecdb3f261ee6a660a11c8d6bcc
WilliamKentoRasmussen <s245310@dtu.dk>
2026-01-13 16:51:56 +0100

    added dockerfile

--- src/my_project/data.py
+++ src/my_project/data.py
@@ -1,27 +1,80 @@
+from __future__ import annotations
+
+import os
+from typing import TYPE_CHECKING
+
 import torch
-import typer
+from torch import Tensor
+from torch.utils.data import Dataset
+"""
+if TYPE_CHECKING:
+    import torchvision.transforms.v2 as transforms
+"""
+import torchvision.transforms.v2 as transforms
+
+class MnistDataset(Dataset):
+    """MNIST dataset for PyTorch.
+
+    Args:
+        data_folder: Path to the data folder.
+        train: Whether to load training or test data.
+        img_transform: Image transformation to apply.
+        target_transform: Target transformation to apply.
+    """
+
+    name: str = "MNIST"
+
+    def __init__(
+        self,
+        data_folder: str = "data",
+        train: bool = True,
+        img_transform: transforms.Transform | None = None,
+        target_transform: transforms.Transform | None = None,
+    ) -> None:
+        super().__init__()
+        self.data_folder = data_folder
+        self.train = train
+        self.img_transform = img_transform
+        self.target_transform = target_transform
+        self.load_data()
+
+    def load_data(self) -> None:
+        """Load images and targets from disk."""
+        images, target = [], []
 
 
-class myDataset(torch.utils.data.Dataset):
-    """Custom Dataset for loading corrupt MNIST data."""
+        """
+        if self.train:
+            nb_files = len([f for f in os.listdir(self.data_folder) if f.startswith("train_images")])
+            for i in range(nb_files):
+                images.append(torch.load(f"{self.data_folder}/train_images_{i}.pt"))
+                target.append(torch.load(f"{self.data_folder}/train_target_{i}.pt"))
+        else:
+            images.append(torch.load(f"{self.data_folder}/test_images.pt"))
+            target.append(torch.load(f"{self.data_folder}/test_target.pt"))
 
-    def __init__(self, data_dir: str) -> None:
-        """Initialize the dataset by loading data from the specified directory."""
-        print("Loading data...")
-        self.images = [0,1,2,3,4,5,6,7]
-        self.labels = [0,1,2,3,4,5,6,7]
-        #self.images = torch.load(f"{data_dir}/train_images.pt")
-        #self.labels = torch.load(f"{data_dir}/train_target.pt")
+        """
+        if self.train:
+            images.append(torch.load(f"{self.data_folder}/train_images.pt"))
+            target.append(torch.load(f"{self.data_folder}/train_target.pt"))
+        else:
+            images.append(torch.load(f"{self.data_folder}/test_images.pt"))
+            target.append(torch.load(f"{self.data_folder}/test_target.pt"))
+        self.images = torch.cat(images, 0)
+        self.target = torch.cat(target, 0)
+
+    def __getitem__(self, idx: int) -> tuple[Tensor, Tensor]:
+        """Return image and target tensor."""
+        img, target = self.images[idx], self.target[idx]
+        if self.img_transform:
+            img = self.img_transform(img)
+        if self.target_transform:
+            target = self.target_transform(target)
+        return img, target
 
     def __len__(self) -> int:
-        """Return the total number of samples in the dataset."""
-        return len(self.labels)
-
-    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
-        """Retrieve a sample and its corresponding label by index."""
-        image = self.images[idx]
-        label = self.labels[idx]
-        return image, label
+        """Return the number of images in the dataset."""
+        return self.images.shape[0]
 
 
 def normalize(images: torch.Tensor) -> torch.Tensor:
